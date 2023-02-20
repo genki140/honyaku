687b2df27d343af3d952ad7f1c1159f2d6eb743b
Genki <genki140@gmail.com>
2023-02-20 07:06:12 +0000

    grpahql追加

--- apps/graphql/src/app.module.ts
+++ apps/graphql/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
+import { PrismaService } from './prisma.service';
+import { PostsResolver } from './posts/posts.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, PostsResolver],
 })
-export class AppModule {}
+export class AppModule { }
